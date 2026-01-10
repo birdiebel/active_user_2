23a42869351a68a569ca12b769915d2039967086
jpb@linux <jp.bourdon.midam@gmail.com>
2026-01-10 08:41:52 +0100

    correct lint 3

--- db/migrate/20260102041438_create_players.rb
+++ db/migrate/20260102041438_create_players.rb
@@ -2,8 +2,8 @@ class CreatePlayers < ActiveRecord::Migration[8.1]
   def change
     create_table :players do |t|
       t.string :firstname, null: false
-      t.string :lastname, null:false
-      t.date :dob, null:false, default: "1970/01/01"
+      t.string :lastname, null: false
+      t.date :dob, null: false, default: "1970/01/01"
       t.integer :sexe, default: 0
       t.integer :lang, default: 0
       t.references :user, null: true
