486c4a6e1505568f08e0ad00daa8510b33f76577
jpb@linux <jp.bourdon.midam@gmail.com>
2026-01-12 18:02:00 +0100

    entries-change

--- app/admin/entries.rb
+++ app/admin/entries.rb
@@ -1,23 +1,45 @@
 ActiveAdmin.register Entry do
   permit_params :event_id, :player_id, :licence_id, :status
 
+  includes :event, :player, :licence
+
   index do
-    selectable_column
-    column :event
-    column :player do |entry|
-      entry.player.full_name
-    end
-    column :licence do |entry|
-      if entry.licence
-        "#{entry.licence.num} (#{entry.licence.club})"
-      else
-        "N/A"
+    table_for Entry.order("created_at DESC") do
+      column :event
+      column :player do |entry|
+        entry.player.full_name
       end
+      column :licence do |entry|
+        if entry.licence
+          "#{entry.licence.num} (#{entry.licence.club})"
+        else
+          "N/A"
+        end
+      end
+      column :status
+      column :created_at
+      column :updated_at
+      actions
     end
-    column :status
-    column :created_at
-    column :updated_at
-    actions
+
+    # table_for Player.all do |player|
+    #     column :id
+    #     column :full_name do |player|
+    #         player.full_name
+    #     end
+    #     column :my_licence do |player|
+    #         player.my_licence
+    #     end
+    #     column :my_club do |player|
+    #         player.my_club
+    #     end
+    #     column :age do |player|
+    #         player.age
+    #     end
+    #     column "Cat" do |player|
+    #         player.age_category
+    #     end
+    # end
   end
 
   form do |f|
